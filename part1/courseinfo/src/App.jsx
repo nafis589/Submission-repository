4fe271356e9f6f10d98e644dd25d5dff37e30fd0
nafis585 <princenafistoure@gmail.com>
2025-10-09 08:29:19 +0000

    1.5 : courseinfo, étape 5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts : [
   {
     name: 'Fundamentals of React',
     exercises: 10,
@@ -15,31 +15,35 @@ const App = () => {
     name: 'State of a component',
     exercises: 14,
   }
-  ];
+  ],
+  }
+  
+
 
 const Header = (props)=>{
     return (
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     )
   }
 
   const Part = (props)=>{
     return (
-      props.parts.map(part=><p>{part.name} {part.exercises}</p>)
+      props.course.parts.map(part=><p key={part.name}>{part.name} {part.exercises}</p>)
+      
     )
   }
 
 const Content = (props)=>{
     return (
       <div>
-        <Part parts={props.parts}/>
+        <Part course={props.course}/>
       </div>
     )
   }
 
   const Total = (props) =>{ 
     let total=0;
-    props.parts.map(part=>total+=part.exercises)
+    props.course.parts.map(part=>total+=part.exercises)
     return (
       <p>Number of exercices {total}</p>
     )
@@ -47,8 +51,8 @@ const Content = (props)=>{
   return (
     <div>
       <Header course= {course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
