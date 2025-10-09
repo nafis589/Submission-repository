d911ab75820e8adbd57b9b4a8a8be739d2cd0700
nafis585 <princenafistoure@gmail.com>
2025-10-09 07:20:16 +0000

    1.4 : courseinfo, étape 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,25 +2,53 @@ import React from 'react'
 const App = () => {
   const course = 'Half Stack application development'
 
-  const part1 ={
+  const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10,
-  }
-  const part2 ={
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7,
-  }
-  const part3 ={
+  },
+  {
     name: 'State of a component',
     exercises: 14,
   }
+  ];
 
+const Header = (props)=>{
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
+
+  const Part = (props)=>{
+    return (
+      props.parts.map(part=><p>{part.name} {part.exercises}</p>)
+    )
+  }
+
+const Content = (props)=>{
+    return (
+      <div>
+        <Part parts={props.parts}/>
+      </div>
+    )
+  }
+
+  const Total = (props) =>{ 
+    let total=0;
+    props.parts.map(part=>total+=part.exercises)
+    return (
+      <p>Number of exercices {total}</p>
+    )
+  }
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
+      <Header course= {course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
