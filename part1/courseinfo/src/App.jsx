cab6569ae7a6c7d22bec636b36934485442cf930
nafis585 <princenafistoure@gmail.com>
2025-10-14 07:44:08 +0000

    1.8 : unicafé, étape3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,20 @@
 import {useState} from 'react'
 
+const Statistics = (props) =>{
+  const all = props.good + props.neutral + props.bad;
+  return(
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {(props.good - props.bad)/all}</p>
+      <p>positive {(props.good/all)*100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -7,20 +22,14 @@ const App = () => {
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
-  const all = good + neutral + bad;
+  
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad)/all}</p>
-      <p>positive {(good/all)*100} %</p>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div> 
   )
 }
