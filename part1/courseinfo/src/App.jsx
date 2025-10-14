3a9ed8501fa4f3417bf7bfb918a01ae111a490fd
nafis585 <princenafistoure@gmail.com>
2025-10-14 07:57:35 +0000

    1.9 : unicafé, étape4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,9 +2,13 @@ import {useState} from 'react'
 
 const Statistics = (props) =>{
   const all = props.good + props.neutral + props.bad;
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
   return(
     <div>
-      <h1>statistics</h1>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
@@ -29,6 +33,7 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
       <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div> 
   )
